2101a2afc87be1adc83b8ada72e9e66b189da4a4
Jason C Rodriguez <jasoncrodriguez@gmail.com>
2017-05-06 16:44:18 -0400

    get sunburst plot to work with cc data

--- napa/colorcommons.py
+++ napa/colorcommons.py
@@ -2,7 +2,11 @@ import pandas as pd
 import re
 import hashlib
 import random
-
+import seaborn as sb
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import json
 
 class UserTexts(object):
 
@@ -18,17 +22,23 @@ class UserTexts(object):
         req_columns = ['From', 'Body', 'SentDate', 'Status']
 
         try:
-            self.data = df[req_columns]
-
+            self.data = df[req_columns] 
         except KeyError as er:
             raise KeyError(er,
                            'Req columns: {}'.format(', '.join(req_columns)))
 
         # use datetime as index
+        self.data['SentDateTime'] = pd.to_datetime(self.data.SentDate)
         self.data.index = pd.to_datetime(self.data.SentDate)
+        self.data.index.tz_localize('UTC').tz_convert('US/Eastern')
 
         if hash_id_table == None:
             self.hash_id_table = 'data/names1880.txt'
+
+
+    def count_unique(self, x):
+        return len(set(x))
+
 
     def cute_hash(self, x, baby_names_df):
 
@@ -43,19 +53,78 @@ class UserTexts(object):
             full_id = ''.join([id_name, suffix])
         return full_id 
 
-    def heatmap(self):
-        pass
+    def to_sunburst(self, outPath, grp='From'):
+        grouped = self.data.groupby(grp)
+
+        parent_json = {}
+        parent_json["name"] = grp
+        parent_json["children"] = []
+
+        for name, group in grouped:
+            child_json = {}
+            child_json["name"] = name.strftime("%Y%m%d")
+            for in group.Body.value_counts()
+            child_json["children"] = group.Body.value_counts()
+
+        print('Sunburst data written to {}'.format(outPath))
+
+
+    def heatmap(self, ptype='all'):
+
+        if ptype == 'all':
+            self.weekday_heatmap(len)
+
+        else:
+            if ptype == 'unique':
+                self.weekday_heatmap(self.count_unique)
+            else:
+                print('{} is not a valid plot type'.format(ptype))
+
 
     def hash_field(self):
         baby_names_df = pd.read_csv(self.hash_id_table, names = ['baby_name', 'gender', 'ct'])
         hashed_id = [self.cute_hash(x, baby_names_df) for x in self.data.From]
         self.data.From = hashed_id
 
+
     def save_data(self, save_path):
         self.data.to_csv(save_path)
 
-    def summary(self):
-        pass
 
-    def show_plot_options(self):
-        pass
+    def summary(self):
+        summary_dict = {}
+
+        # Total messages
+        summary_dict['total_msg'] = len(self.data)
+
+        # unique user count
+        summary_dict['unique_users'] = len(set(self.data.From))
+
+        summary_dict['repeat_users'] = sum(self.data.From.value_counts() > 1)
+
+        summary_dict['dedicated_users'] = sum(self.data.From.value_counts() > 10)
+
+        summary_dict['top_colors'] = self.data.Body.str.lower().value_counts().head(5).to_json()
+
+        summary_dict['secret_texts'] = sum(self.data.Body.str.lower() == 'secret')
+        return summary_dict
+
+
+    def weekday_heatmap(self, _aggfunc):
+        # Use pandas pivot table to sum up counts by weekday and hour
+        # This creates a 2D dataframe containing the counts
+
+        plot_df = self.data
+
+        plot_df['hour'] = plot_df.SentDateTime.dt.hour
+        plot_df['weekday'] = plot_df.SentDateTime.dt.weekday_name
+
+
+        weekday_hour = pd.pivot_table(plot_df, values='From', index=['hour'], columns=['weekday'], aggfunc=_aggfunc, fill_value=0)
+    
+        # Overwrite column names with abbrev
+        weekday_hour.columns = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    
+        # Create a heat plot with our 2D dataframe
+        sb.heatmap(weekday_hour)
+        plt.show()
